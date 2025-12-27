85764198b98f799ddd6c3dcc20e24772dd231ee1
Madin357 <madin.qahramanov@gmail.com>
2025-12-27 20:17:04 +0000

    2

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-# Flush existing rules
-sudo iptables -F
-sudo ip6tables -F
-# Allow incoming TCP traffic on port 80
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-# Drop all other incoming traffic
-sudo iptables -A INPUT -j DROP
-sudo ip6tables -A INPUT -j DROP
-echo "Rules updated"
-echo "Rules updated (v6)"
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT && sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated" && echo "Rules updated (v6)"
